c42b83d9e1cffecda24e7079e7d7990b12ad61ff
cakaaaXs <viviansaraiva615@gmail.com>
2021-02-18 01:03:45 -0300

    first commit

---
+++ ptzeus-master/settings/config.js
@@ -0,0 +1,4 @@
+const config = {
+        botName: 'Cakabot',
+        ownerName: 'Cakaaax',
+}
---
+++ ptzeus-master/src/zeusmenu.js
@@ -0,0 +1,12 @@
+const cakaaaxmenu = (prefix, pushname) => {
+    return `❍ *Comandos do cakaaax*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.cakaaaxmenu = cakaaaxmenu
